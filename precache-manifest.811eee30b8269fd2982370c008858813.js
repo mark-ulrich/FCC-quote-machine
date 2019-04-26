07168256feac733b746844af5f34bcfcd5107be6
Mark Ulrich <mark.ulrich.86@gmail.com>
2019-04-26 07:41:58 -0500

    Updates

---
+++ precache-manifest.811eee30b8269fd2982370c008858813.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3982c5385da9b4c993a199cf5fff07b",
+    "url": "/FCC-quote-machine/index.html"
+  },
+  {
+    "revision": "b2a943467d931efa3224",
+    "url": "/FCC-quote-machine/static/css/main.596ad8d0.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/FCC-quote-machine/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "b2a943467d931efa3224",
+    "url": "/FCC-quote-machine/static/js/main.1659bc73.chunk.js"
+  },
+  {
+    "revision": "f2d3055fab43bb095058",
+    "url": "/FCC-quote-machine/static/js/runtime~main.e56c3af7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d9843a9ed02a857543d31d35bc72aba5.js"
+  "/FCC-quote-machine/precache-manifest.811eee30b8269fd2982370c008858813.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FCC-quote-machine/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
